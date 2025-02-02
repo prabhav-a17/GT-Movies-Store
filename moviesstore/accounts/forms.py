bc4b140d14cbb70bb6c68efa749eaa40dd89eeec
prabhav-a17 <prabhavagrawal123@gmail.com>
2025-02-02 15:57:56 -0500

    Update forms.py

--- moviesstore/accounts/forms.py
+++ moviesstore/accounts/forms.py
@@ -10,8 +10,7 @@ class CustomErrorList(ErrorList):
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__ (*args, **kwargs)
-        for fieldname in ['username', 'password1',
-        'password2']:
+        for fieldname in ['username', 'password1','password2']:
             self.fields[fieldname].help_text = None
             self.fields[fieldname].widget.attrs.update(
                 {'class': 'form-control'}
