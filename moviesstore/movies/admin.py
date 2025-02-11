b66913ed713f5b41bf294ea36525533b82f04840
riyasoneji <rsoneji6@gatech.edu>
2025-02-11 18:19:31 -0500

    adding gitignore

--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Movie, Review
+from django.contrib import admin
+from .models import Movie
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+admin.site.register(Movie, MovieAdmin)
+
 admin.site.register(Movie)
 
 # Register your models here.
